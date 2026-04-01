324af10cd162c05115db1a43311353c20156a3a5
SultanDev <sultanrootaws@gmail.com>
2026-04-01 21:50:46 +0700

    remove .next build cache from tracking

--- .next/dev/server/chunks/ssr/_next-internal_server_app_berita_[slug]_page_actions_0bb4214f.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/berita/[slug]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_berita_%5Bslug%5D_page_actions_0bb4214f.js.map
--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__2a7c550b._.css",
-  "static/chunks/_94a8fd96._.js"
-],
-    source: "dynamic"
-});
